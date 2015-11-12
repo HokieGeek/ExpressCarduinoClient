61aa63e2d73fbdd0ff627a712a11750f62956ba3
HokieGeek <andres.f.perez@gmail.com>
2015-11-12 15:37:42 -0500

    Putting together the initial skeleton

--- expresscarduino/connection.go
+++ expresscarduino/connection.go
@@ -1,7 +1,29 @@
 package expresscarduino
 
+import (
+    "os"
+    "syscall"
+    // "time"
+    // "unsafe"
+)
+
+const initialBaudRate uint32 = syscall.B9600
+
 type Serial struct {
+    deviceName string
 }
 
-func Connect(Serial ser) {
+type Connection struct {
+    file *os.File
+}
+
+func (c *Connection) Connect() (error) {
+    // Open the file
+    // do an IOCTL call to set the parity and initial baud rate
+    // Perform handshake with EC
+    return nil
+}
+
+func New(ser Serial) (*Connection, error) {
+    return nil, nil
 }
